dc7ed45ed17103386b64aeb08ff676a2fea4cfdf
Agustina Fassina | ENG <agustinafassina@gmail.com>
2025-10-24 20:12:09 -0300

    variables rename in auth0 access review

--- Auth0/AccessToken.bash
+++ Auth0/AccessToken.bash
@@ -1,23 +1,24 @@
 #!/bin/bash
+# v2.0
 ## Auth0 example:
-auth0ExternalApp=""
-auth0Audience=""
-auth0ClientId=""
-auth0ClientSecret=""
+auth0_external_app=""
+auth0_audience=""
+auth0_client_id=""
+auth0_client_secret=""
 
-declare -r bodyJson='{
-    "audience":"'"$auth0Audience"'",
-    "client_id":"'"$auth0ClientId"'",
-    "client_secret":"'"$auth0ClientSecret"'",
+declare -r body_json='{
+    "audience":"'"$auth0_audience"'",
+    "client_id":"'"$auth0_client_id"'",
+    "client_secret":"'"$auth0_client_secret"'",
     "grant_type":"client_credentials"
 }'
 
-codeResponse="$(curl --request POST --url $auth0ExternalApp --header "Content-type:application/json" --data "${bodyJson}" -w '\n%{http_code}' --output response.txt)"
+code_response="$(curl --request POST --url $auth0_external_app --header "Content-type:application/json" --data "${body_json}" -w '\n%{http_code}' --output response.txt)"
 
-if [ "$codeResponse" -eq "403" ];then
+if [ "$code_response" -eq "403" ];then
     bodyResponse="'" cat response.txt"'"
 fi
 
-if [ "$codeResponse" -eq "201" ];then
-    codeResponse="'" cat response.txt"'"
+if [ "$code_response" -eq "201" ];then
+    code_response="'" cat response.txt"'"
 fi
